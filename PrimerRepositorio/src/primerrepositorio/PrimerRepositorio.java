e4b9701263abce7ebdf9d25cca454db6edd10e74
LuisRuiz16 <62570482+LuisRuiz16@users.noreply.github.com>
2020-04-02 15:07:47 -0600

    Update PrimerRepositorio.java

--- PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -20,6 +20,10 @@ public class PrimerRepositorio {
         System.out.println("Dirección: 5av 3-67 Zona 6");
         System.out.println("Telefono: 55748374");
         
+        System.out.println("Nombre: Luis Humberto Ruiz Castillo");
+        System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
+        System.out.println("Telefono: 37070944");
+        
     }
     
 }
