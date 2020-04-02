52c4600ce2ad493b0739fa11a41b48406946ce38
marvin502 <62572934+marvin502@users.noreply.github.com>
2020-04-02 15:40:49 -0600

    Update PrimerRepositorio.java

--- PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -27,6 +27,9 @@ public class PrimerRepositorio {
         
         System.out.println("Nombre: Denis Joel Cifuentes Cano");
         System.out.println("Direccion: Zona 1, Chiantla Huehuetenango");
+        System.out.println("Direccion: Zona 1, Chiantla Huehuetenango");
+        System.out.println("Telefono: 45883960");
+        
         System.out.println("Telefono: 45883960");
         
         System.out.println("Nombre: " + "Abner Obed Domínguez Rodríguez");
@@ -36,6 +39,10 @@ public class PrimerRepositorio {
         System.out.println("Carlos Andrés Miranda Méndez");
         System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
         System.out.println("58385781");
+        
+        System.out.println("Marvin Arnoldo Ordoñez Jimenez");
+        System.out.println("Zona 8, Huehuetenango");
+        System.out.println("48548846");
 
         
     }
