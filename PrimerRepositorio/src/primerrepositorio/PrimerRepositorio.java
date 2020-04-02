d1a285030e03efa9f8225be0db186220c0e79106
GeovanniArguetaAnzueto <62572249+GeovanniArguetaAnzueto@users.noreply.github.com>
2020-04-02 15:23:23 -0600

    Update PrimerRepositorio.java

--- PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -17,6 +17,10 @@ public class PrimerRepositorio {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        System.out.println("Nombre: Geovanni Alfredo Argueta Anzueto");
+        System.out.println("Dirección: 5av 3-67 Zona 6");
+        System.out.println("Telefono: 55748374");
+        
         System.out.println("Nombre: Luis Humberto Ruiz Castillo");
         System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
         System.out.println("Telefono: 37070944");
@@ -24,10 +28,6 @@ public class PrimerRepositorio {
         System.out.println("Nombre: Denis Joel Cifuentes Cano");
         System.out.println("Direccion: Zona 1, Chiantla Huehuetenango");
         System.out.println("Telefono: 45883960");
-        
-        System.out.println("Nombre: Geovanni Alfredo Argueta Anzueto");
-        System.out.println("Dirección: 5av 3-67 Zona 6");
-        System.out.println("Telefono: 55748374");
         
         System.out.println("Nombre: " + "Abner Obed Domínguez Rodríguez");
         System.out.println("Dirección: " + "Colonia El Oasis, Zona 8, Huehuetenango");
