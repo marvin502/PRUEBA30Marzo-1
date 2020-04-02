a4437507a822a6791a565bd971185b69bcda06b5
hpabnerdom <abnerdomhp@hotmail.com>
2020-04-02 15:09:03 -0600

    Update PrimerRepositorio.java

--- PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -20,9 +20,9 @@ public class PrimerRepositorio {
         System.out.println("Dirección: 5av 3-67 Zona 6");
         System.out.println("Telefono: 55748374");
         
-        System.out.println("Nombre: Luis Humberto Ruiz Castillo");
-        System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
-        System.out.println("Telefono: 37070944");
+        System.out.println("Nombre: " + "Abner Obed Domínguez Rodríguez");
+        System.out.println("Dirección: " + "Colonia El Oasis, Zona 8, Huehuetenango");
+        System.out.println("Telefono: " + "42252923");
         
     }
     
