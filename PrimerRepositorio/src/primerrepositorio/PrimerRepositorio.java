9ecfebea9bf9bb60e82886d683cc5ed27c60e6f8
LuisRuiz16 <62570482+LuisRuiz16@users.noreply.github.com>
2020-04-02 15:12:35 -0600

    Update PrimerRepositorio.java

--- PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -16,6 +16,15 @@ public class PrimerRepositorio {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Nombre: Luis Humberto Ruiz Castillo");
+        System.out.println("Direccion: 8va Calle A 11-30 zona 1 interior");
+        System.out.println("Telefono: 37070944");
+        
+        System.out.println("Nombre: Denis Joel Cifuentes Cano");
+        System.out.println("Direccion: Zona 1, Chiantla");
+        System.out.println("Telefono: 45883960");
+        
         System.out.println("Nombre: Geovanni Alfredo Argueta Anzueto");
         System.out.println("Dirección: 5av 3-67 Zona 6");
         System.out.println("Telefono: 55748374");
