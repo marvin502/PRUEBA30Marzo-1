c2cabf9a588f5448f839c572a6ca125c31cd8220
marvin502 <62572934+marvin502@users.noreply.github.com>
2020-04-02 15:54:03 -0600

    Update PrimerRepositorio.java

--- PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
+++ PrimerRepositorio/src/primerrepositorio/PrimerRepositorio.java
@@ -36,6 +36,10 @@ public class PrimerRepositorio {
         System.out.println("Carlos Andrés Miranda Méndez");
         System.out.println("1a Calle A, Colonia Villas del Magisterio Zona 9");
         System.out.println("58385781");
+        
+        System.out.println("Marvin Arnoldo Ordoñez Jimenez");
+        System.out.println("zona 8, Huehuetenango");
+        System.out.println("48548845");
 
         
     }
